a872d3a5bf015caf94ee54793b6f6044d0fbac6d
Michał <147628252+Majkel054@users.noreply.github.com>
2025-01-26 15:07:18 +0100

    Update Program.cs

--- ConsoleApp_26.01.2025/Program.cs
+++ ConsoleApp_26.01.2025/Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp_26._01._2025
 {
     internal class Program
     {
+        static string checkKlasyf(string tekst)
+        {
+            string pattern = @"\bklasyfikacja\b"; //b- granica słowa
+            Match match = Regex.Match(tekst, pattern, RegexOptions.IgnoreCase);
+
+            if (match.Success) 
+            {
+                return "ostatnie zajecia!";
+            }
+            else
+            {
+                return "kontynuuj nauke";
+            }
+        }
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Wprowadz tekst do sprawdzenia: ");
+            string tekst = Console.ReadLine();
+
+            string wynik = checkKlasyf(tekst);
+            Console.WriteLine(wynik);
+
+            Console.WriteLine("Wciśnij dowolny klawisz aby zamknąć program");
+            Console.ReadKey();
         }
     }
 }
